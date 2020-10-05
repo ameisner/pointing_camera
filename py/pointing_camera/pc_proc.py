4846c3b1ef05f213a7fd23d46056014faa4c9390
Aaron Meisner <ameisner@fas.harvard.edu>
2020-10-05 09:38:00 -0700

    initial; pipeline driver script

---
+++ py/pointing_camera/pc_proc.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import argparse
+from datetime import datetime
+import os
+import time
+
+def pc_proc(fname_in):
+
+    print('Starting pointing camera reduction pipeline at: ' +
+          str(datetime.utcnow()) + ' UTC')
+
+    t0 = time.time()
+
+    try:
+        print('Running on host: ' + str(os.environ.get('HOSTNAME')))
+    except:
+        print('Could not retrieve hostname!')
+
+    assert(os.path.exists(fname_in))
+
+    dt = time.time() - t0
+    print('pointing camera reduction pipeline took ' + '{:.2f}'.format(dt) +
+          ' seconds')
+    print('pointing camera reduction pipeline completed at: ' +
+          str(datetime.utcnow()) + ' UTC') 
+
+if __name__ == "__main__":
+    descr = 'run the pointing camera reduction pipeline on an exposure'
+    
+    parser = argparse.ArgumentParser(description=descr)
+
+    parser.add_argument('fname_in', type=str, nargs=1)
+
+    args = parser.parse_args()
+    
+    pc_proc(args.fname_in[0])
