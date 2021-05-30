b798c559b763873b71dd5de48f7fe460ed851302
Aaron Meisner <ameisner@fas.harvard.edu>
2021-05-29 23:01:06 -0700

    clean up

--- py/pointing_camera/exposure.py
+++ py/pointing_camera/exposure.py
@@ -12,30 +12,30 @@ class PC_exposure:
         util._check_bitpix(h)
 
         self.obs_night = util.get_obs_night(h['DATE'], h['TIME'])
-        
+
         # image file name
         self.fname_im = fname_im
 
         # pixel data
         self.raw_image = im
-        
+
         # image header
         self.header = h
 
         # wcs filename
         self.fname_wcs = util.get_wcs_filename(fname_im)
-        
+
         wcs, wcs_header = util.load_wcs(self.fname_wcs)
 
         util._validate_ctype(wcs)
 
         # wcs_header
         self.wcs_header = wcs_header
-        
+
         # wcs object
         self.wcs = wcs
 
         # exposure time in seconds
         self.time_seconds = util.get_exptime(self.header)
-        
+
         self.is_detrended = False
