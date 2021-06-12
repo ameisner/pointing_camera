3ba5bad98f87fa4516c340cebeae0c663c34c2d3
Aaron Meisner <ameisner@fas.harvard.edu>
2021-06-12 02:37:40 -0700

    experiment with accessing pointing camera analysis results via redis

---
+++ bin/readback_redis.py
@@ -0,0 +1,22 @@
+
+import redis
+import os
+import time
+
+host = os.environ['REDISHOST']
+port = int(os.environ['REDISPORT'])
+db = int(os.environ['REDISDBNUM'])
+key = os.environ['REDISKEY']
+
+r = redis.Redis(host=host, port=port, db=db)
+
+
+delay_seconds = 5
+
+for i in range(300):
+    # try hgetall later
+    time.sleep(delay_seconds)
+    data = r.hgetall(key)
+
+    print(data[b'timestamp'].decode(), float(data[b'zp_adu_per_s']), float(data[b'sky_adu_per_s']), i)
+
