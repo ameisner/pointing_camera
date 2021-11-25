89f409c14151c82fbb1e87d64e301bc30e1fd4f9
Aaron Meisner <ameisner@fas.harvard.edu>
2021-11-25 15:12:39 -0800

    option to skip sending telemetry to Redis

--- bin/pc_realtime.py
+++ bin/pc_realtime.py
@@ -10,7 +10,7 @@ default_data_dir = '/global/cscratch1/sd/ameisner/pointing_camera/nino' # NERSC
 
 files_processed = []
 
-def _reduce_new_files(flist_wcs, outdir='.'):
+def _reduce_new_files(flist_wcs, outdir='.', dont_send_redis=False):
 
     for f_wcs in flist_wcs:
         # check that the corresponding .fits raw image exists
@@ -28,12 +28,12 @@ def _reduce_new_files(flist_wcs, outdir='.'):
 
         try:
             pipeline.pc_proc(f_fits, outdir=outdir, dont_write_detrended=True,
-                             skip_checkplot=False, nightly_subdir=True, send_redis=True,
+                             skip_checkplot=False, nightly_subdir=True, send_redis=(not dont_send_redis),
                              one_aper=True, nmp=8)
         except:
             print('PROCESSING FAILURE: ' + f_fits)
 
-def _proc_new_files(data_dir=default_data_dir, outdir='.'):
+def _proc_new_files(data_dir=default_data_dir, outdir='.', dont_send_redis=False):
 
     print('Checking for new .wcs files...')
 
@@ -56,16 +56,17 @@ def _proc_new_files(data_dir=default_data_dir, outdir='.'):
         flist_wcs_new = list(flist_wcs_new)
         flist_wcs_new.sort()
 
-        _reduce_new_files(flist_wcs_new, outdir=outdir)
+        _reduce_new_files(flist_wcs_new, outdir=outdir, dont_send_redis=dont_send_redis)
 
         files_processed = files_processed + flist_wcs_new
 
-def _watch(wait_seconds=5, data_dir=default_data_dir, outdir='.'):
+def _watch(wait_seconds=5, data_dir=default_data_dir, outdir='.',
+           dont_send_redis=False):
 
     while True:
         print('Waiting', wait_seconds, ' seconds')
         time.sleep(wait_seconds)
-        _proc_new_files(data_dir=data_dir, outdir=outdir)
+        _proc_new_files(data_dir=data_dir, outdir=outdir, dont_send_redis=dont_send_redis)
 
 def _do_veto(fname):
     assert(os.path.exists(fname))
@@ -100,6 +101,10 @@ if __name__ == "__main__":
     parser.add_argument('--veto_list', default=None, type=str,
                         help="list of files not to process")
 
+    parser.add_argument('--dont_send_redis', default=False,
+                        action='store_true',
+                        help="don't send results to Redis")
+
     args = parser.parse_args()
 
     assert(args.wait_seconds >= 1)
@@ -108,4 +113,4 @@ if __name__ == "__main__":
         _do_veto(args.veto_list)
 
     _watch(wait_seconds=args.wait_seconds, data_dir=args.data_dir,
-           outdir=args.outdir)
+           outdir=args.outdir, dont_send_redis=args.dont_send_redis)
