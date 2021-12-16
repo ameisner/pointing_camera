815e1c2499439baed9f083a8dcfe2df6903a97d1
Aaron Meisner <ameisner@fas.harvard.edu>
2021-12-15 18:01:50 -0700

    return query result as a list of dictionary-like objects rather than a list of tuples

--- bin/db_sql_example.py
+++ bin/db_sql_example.py
@@ -1,11 +1,12 @@
 import DOSlib.exposure as exp
 import psycopg2 as psycopg
+import psycopg2.extras
 
 sql = 'SELECT id, mjd_obs, exptime, reqra, reqdec, skyra, skydec, targtra, targtdec FROM exposure WHERE exposure.night = 20210611'
 
 conn =  psycopg.connect(exp.dsn)
 
-cursor = conn.cursor()
+cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
 cursor.execute(sql)
 
